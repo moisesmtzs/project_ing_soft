76831fa1a630bfd8cdc2479f527f289e9fc9c116
Josafat Jauregui <alfonso.lopez5658@alumnos.udg.mx>
2021-11-20 17:34:00 -0600

    Agregado el modelo Perfil que se relaciona con el Usuario. Pendiente agregar que el usuario pueda modificar sus datos de perfil

--- wikiprofes/wkp/settings.py
+++ wikiprofes/wkp/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRYSPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'inicio'
 LOGIN_URL = 'login'
