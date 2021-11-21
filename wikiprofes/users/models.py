bafb9d5cfaf4f763e7ccd18f0249b2ae227f7251
Josafat Jauregui <alfonso.lopez5658@alumnos.udg.mx>
2021-11-20 19:43:14 -0600

    Usuario puede modificar su nombre, correo y foto de perfil

--- wikiprofes/users/models.py
+++ wikiprofes/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -9,3 +10,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 250 or img.width > 250:
+            output_size = (250,250)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
