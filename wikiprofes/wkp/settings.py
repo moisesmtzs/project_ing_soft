38e8add079ad1acbccca3ad82dfae8e357abd334
Josafat Jauregui <alfonso.lopez5658@alumnos.udg.mx>
2021-11-21 22:36:13 -0600

    Servicio de envio de correos agregados para el correo presente en el archivo Datos Prueba. Vistas de recuperar contraseña. Me imagino que no funciona para otros correos, tendria que revisar documentacion, pero de momento messirve

--- wikiprofes/wkp/settings.py
+++ wikiprofes/wkp/settings.py
@@ -140,9 +140,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.stmp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'progint123@gmail.com'
-EMAIL_HOST_PASSWORD = 'hBvFQXt2G-G-D7_'
+EMAIL_HOST_PASSWORD = 'ejnvsliwflhnragx'
