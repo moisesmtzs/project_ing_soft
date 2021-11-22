be75d609ed8a565b10243563467dd0d147d959e2
moisesmtzs <moises.martinez6605@alumnos.udg.mx>
2021-11-21 20:53:30 -0600

    Autenticación de correo

--- wikiprofes/wkp/urls.py
+++ wikiprofes/wkp/urls.py
@@ -36,7 +36,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 
-    path('create_profesor/', create_profesor, name='create_profesor')
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name="password_reset"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
+    path('password-reset-confirm/<uidb>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
+    
+    path('create-profesor/', create_profesor, name='create_profesor')
 ]
 
 if settings.DEBUG:
